b9566a3f490153880dc8956de721ce47296b889d
Adam Norton <adam3norton@gmail.com>
2023-02-13 19:25:14 -0700

    Add project files.

---
+++ Mission6_anorto-1/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission6_anorto_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6_anorto_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieContext _movieContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, MovieContext movies)
+        {
+            _logger = logger;
+            _movieContext = movies;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult MovieForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieForm(MovieRecord movie)
+        {
+            _movieContext.Add(movie);
+            _movieContext.SaveChanges();
+            return View("Confirmation", movie);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission6_anorto-1/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mission6_anorto_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission6_anorto_1
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base (options) {
+            // blank for now
+        }
+
+        public DbSet<MovieRecord> Movies { get; set; }
+    }
+}
